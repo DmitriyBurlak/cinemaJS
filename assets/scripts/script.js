374e6f3e868f69f63009f8fb36d51d6b90bbdece
Dmitriy <dmshade25@gmail.com>
2020-06-01 21:02:12 +0300

    second commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,29 +1,34 @@
 let hall = document.querySelector('.application');
 class Chair {
-    id = 'null';
+    id = '0';
     color = '#ffffff';
     width = '50px';
     height = '50px';
-    top = '170';
-    left = '135';
+    top = '135';
+    left = '170';
 
-    createChair() {
+    createChair(top, left) {
         let chair = document.createElement('div');
         chair.classList.add('chair');
         chair.style.backgroundColor = this.color;
         chair.style.width = this.width;
         chair.style.height = this.height;
-        chair.style.left = this.top + 'px';
-        chair.style.top = this.left + 'px';
+        chair.style.left = left + 'px';
+        chair.style.top = top + 'px';
         hall.append(chair);
     }
 }
 
-
-
+let chair1 = new Chair();
+chair1.createChair(135, 220);
 let chair2 = new Chair();
-chair2.createChair();
+chair2.createChair(135, 155);
 
+let chairs = document.querySelectorAll('.chair');
+chairs.forEach((item, i) => {
+    item.id = i;
+})
+console.log('ch: ', chairs);
 
 
 
